eb0fe3266a0e93be703d21214d1acd5f4bf5c3d5
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-03-15 09:00:11 +0100

    Initial commit

---
+++ src/main/java/com/example/clausgithubtraining/ClausGitHubTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.clausgithubtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClausGitHubTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClausGitHubTrainingApplication.class, args);
+    }
+
+}
